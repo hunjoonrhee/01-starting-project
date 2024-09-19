4b91617ec29264028793c949244c8e3726f0df10
hunjoonrhee <hunjoonrhee90@gmail.com>
2024-09-19 23:00:47 +0200

    goal is not showing users randomly.

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,4 @@
-import { DUMMY_USERS } from './../data/dummy-users';
-import { Component, computed, signal } from '@angular/core';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -11,16 +8,7 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-
-  // get imagePath() {
-  //   return 'assets/users/' + this.selectedUser.avatar
-  // }
-
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
-
   onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+
   }
 }
