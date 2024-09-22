b1c3fd8884c01b1c28ecfda677e9b81a971a6f23
hunjoonrhee <hunjoonrhee90@gmail.com>
2024-09-22 22:50:45 +0200

    formModule, two way binding

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,14 +1,18 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css',
 })
 export class NewTaskComponent {
   @Output() cancelAddingNewTask = new EventEmitter<boolean>();
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDueDate = '';
 
   onCancelAddingNewTask() {
     this.cancelAddingNewTask.emit(false);
