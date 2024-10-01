ef86f357200a662a381e8c7b352a2961ddf5bfdd
hunjoonrhee <hunjoonrhee90@gmail.com>
2024-10-01 10:27:05 +0200

    add service

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -6,6 +6,13 @@ import { NewTask } from '../models/task';
 export class TasksService {
   tasks = DUMMY_TASKS;
 
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
   getUserTasks(userId: string) {
     return this.tasks.filter((task) => task.userId === userId);
   }
@@ -18,9 +25,16 @@ export class TasksService {
       summary: taskData.summary,
       dueDate: taskData.dueDate,
     });
+    this.saveTasks();
   }
 
   onDeleteTask(taskId: string) {
     this.tasks = this.tasks.filter((task) => task.id !== taskId);
+    this.saveTasks();
+  }
+
+  private saveTasks() {
+    const tasks = JSON.stringify(this.tasks);
+    localStorage.setItem('tasks', tasks);
   }
 }
